9a63a35db0568d87431c825e9a03b8b799ee992f
tuxredux <tuxredux2@gmail.com>
2017-04-11 16:10:44 -0500

    Update cookiecutter.json,post_gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 
 import os
 import json
+import shutil
 import base64
 import urllib
 import urllib2
@@ -12,39 +13,51 @@ import getpass
 import subprocess
 
 REPO_PATH = os.getcwd()
+LICENSES_DIRPATH = os.path.join(REPO_PATH, 'LICENSES')
+LICENSE_TXT = os.path.join(LICENSES_DIRPATH, '{{cookiecutter.license}}.txt')
+LICENSE_PATH = os.path.join(REPO_PATH, 'LICENSE')
 NOTICE_PATH = os.path.join(REPO_PATH, 'NOTICE')
 GITIGNORE_PATH = os.path.join(REPO_PATH, '.gitignore')
 GITIGNORE_URL = 'https://www.gitignore.io/api/{{cookiecutter.gitignore}}'
+JSON_HEADER = {'Content-Type': 'application/json; charset=utf-8'}
 
-{% if cookiecutter.git_user == cookiecutter.repo_space %}
+BITBUCKET_REPOS_URL = ('https://api.bitbucket.org/2.0/repositories/'
+    '{{cookiecutter.cookiecutter_namespace}}/{{cookiecutter.cookiecutter_slug}}')
+
+{% if cookiecutter.git_username == cookiecutter.repository_namespace %}
 GITHUB_REPOS_URL = 'https://api.github.com/user/repos'
 {% else %}
-GITHUB_REPOS_URL = 'https://api.github.com/orgs/{{cookiecutter.repo_space}}/repos'
+GITHUB_REPOS_URL = ('https://api.github.com/orgs/'
+    '{{cookiecutter.cookiecutter_namespace}}/repos')
 {% endif %}
 
-{% if cookiecutter.remote_provider == 'GitLab' %}
+{% if cookiecutter.remote_provider == 'gitlab.com' %}
 GITLAB_TOKEN = getpass.getpass('gitlab_token: ').strip()
 GITLAB_TOKEN_HEADER = {'PRIVATE-TOKEN': GITLAB_TOKEN}
 {% endif %}
 GITLAB_NAMESPACES_URL = 'https://gitlab.com/api/v3/namespaces'
 GITLAB_PROJECTS_URL = 'https://gitlab.com/api/v3/projects'
-REPO_REMOTE_URL = ('https://{{cookiecutter.git_user}}@'
-    '{{cookiecutter.remote_provider|lower}}.com/'
-    '{{cookiecutter.repo_space}}/{{cookiecutter.repo_name}}.git')
-REPO_NAME_DATA = {'name': '{{cookiecutter.repo_name}}'}
+
+REMOTE_REPO_URL = ('https://{{cookiecutter.git_username}}@'
+    '{{cookiecutter.remote_provider}}/{{cookiecutter.cookiecutter_namespace}}/'
+    '{{cookiecutter.cookiecutter_slug}}.git')
+REMOTE_REPO_DATA = {
+    'name': '{{cookiecutter.cookiecutter_slug}}',
+    'description': '{{cookiecutter.cookiecutter_description}}'
+    }
 
 
 def run(command, log=True):
     try:
         output = subprocess.check_output(command)
     except subprocess.CalledProcessError as error:
-        print '%s: %s\n%s' % (error.returncode, error.cmd, error.output)
+        print('{}: {}\n{}'.format(error.returncode, error.cmd, error.output))
         raise error
     else:
         if output and log:
-            print '%s\n%s' % (' '.join(command), output)
+            print('{}\n{}'.format(' '.join(command), output))
         else:
-            print ' '.join(command)
+            print(' '.join(command))
     return output
 
 
@@ -57,78 +70,98 @@ def _request(url, headers, data, log):
     except urllib2.HTTPError as error:
         message = error.read()
         error.close()
-        print '%s %s: %s\n%s' % (error.code, error.reason, url, message)
-        raise error
+        print('{} {}: {}\n{}'.format(error.code, error.reason, url, message))
+        raise SystemExit
     else:
         if content and log:
-            print '%s\n%s' % (url, content)
+            print('{}\n{}'.format(url, content))
         else:
-            print url
+            print(url)
     return content
 
 
-def get(url, headers={}, log=True):
-    return _request(url, headers, None, log)
+class requests(object):
+
+    @staticmethod
+    def get(url, headers={}, log=True):
+        return _request(url, headers, None, log)
+
+    @staticmethod
+    def post(url, headers={}, data=None, log=True):
+        return _request(url, headers, data, log)
 
 
-def post(url, headers={}, data=None, log=True):
-    return _request(url, headers, data, log)
+def create_remote_repo():
+    {% if cookiecutter.remote_provider == 'github.com' %}
+    data = json.dumps(REMOTE_REPO_DATA)
+    prompt = ("Password for 'https://{{cookiecutter.git_username}}@"
+        "{{cookiecutter.remote_provider}}': ")
+    auth_info = ('{{cookiecutter.git_username}}',
+                 getpass.getpass(prompt).strip())
+    auth_base = base64.b64encode('{}:{}'.format(*auth_info))
+    headers = {'Authorization': 'Basic {}'.format(auth_base)}
+    requests.post(GITHUB_REPOS_URL, data=data, headers=headers)
 
-
-def create_github_repo():
-    data = json.dumps(REPO_NAME_DATA)
-    prompt = (u"Password for 'https://{{cookiecutter.git_user}}@"
-        u"{{cookiecutter.remote_provider|lower}}.com': ")
-    auth_info = ('{{cookiecutter.git_user}}', getpass.getpass(prompt).strip())
-    auth_base = base64.b64encode('%s:%s' % auth_info)
-    headers = {'Authorization': 'Basic %s' % auth_base}
-    post(GITHUB_REPOS_URL, data=data, headers=headers)
-
-
-def create_gitlab_repo():
-    search_param = {'search': '{{cookiecutter.repo_space}}'}
+    {% elif cookiecutter.remote_provider == 'gitlab.com' %}
+    search_param = {'search': '{{cookiecutter.cookiecutter_namespace}}'}
     search_url = GITLAB_NAMESPACES_URL + '?' + urllib.urlencode(search_param)
-    search_results = get(search_url, headers=GITLAB_TOKEN_HEADER)
+    search_results = requests.get(search_url, headers=GITLAB_TOKEN_HEADER)
     gitlab_namespaces = json.loads(search_results)
     for namespace in gitlab_namespaces:
-        if namespace.get('name', '') == '{{cookiecutter.repo_space}}':
+        if namespace.get('name', '') == '{{cookiecutter.cookiecutter_namespace}}':
             namespace_id = namespace.get('id', '')
     if namespace_id:
-        REPO_NAME_DATA.update({'namespace_id': namespace_id})
-    data = unicode(urllib.urlencode(REPO_NAME_DATA))
-    post(GITLAB_PROJECTS_URL, data=data, headers=GITLAB_TOKEN_HEADER)
+        REMOTE_REPO_DATA.update({'namespace_id': namespace_id})
+    data = unicode(urllib.urlencode(REMOTE_REPO_DATA))
+    requests.post(GITLAB_PROJECTS_URL, data=data, headers=GITLAB_TOKEN_HEADER)
 
-{% if cookiecutter.license != 'Apache-2.0' %}
-print u"Removing '%s'..." % NOTICE_PATH
-os.remove(NOTICE_PATH)
-{% endif %}
+    {% elif cookiecutter.remote_provider == 'bitbucket.org' %}
+    REMOTE_REPO_DATA.update({'has_issues': True, 'is_private': True})
+    data = json.dumps(REMOTE_REPO_DATA)
+    prompt = ("Password for 'https://{{cookiecutter.git_username}}@"
+        "{{cookiecutter.remote_provider}}': ")
+    auth_info = ('{{cookiecutter.git_username}}',
+                 getpass.getpass(prompt).strip())
+    auth_base = base64.b64encode('{}:{}'.format(*auth_info))
+    JSON_HEADER['Authorization'] = 'Basic {}'.format(auth_base)
+    requests.post(BITBUCKET_REPOS_URL, data=data, headers=JSON_HEADER)
+    {% endif %}
 
-{% if cookiecutter.gitignore != 'windows,osx,linux,git' %}
-with open(GITIGNORE_PATH, 'wb') as f:
-    f.write(get(GITIGNORE_URL))
-print u"updated '%s'" % GITIGNORE_PATH
-{% endif %}
 
-run(['git', 'init'])
-run(['git', 'status'])
-run(['git', 'add', '-A'])
-run(['git', 'status'])
-run(['git', 'commit', '-m', 'Initial commit'])
+def setup_license_file():
+    {% if cookiecutter.license != 'Apache-2.0' %}
+    print("Removing '{}'...".format(NOTICE_PATH))
+    os.remove(NOTICE_PATH)
+    {% endif %}
+    shutil.move(LICENSE_TXT, LICENSE_PATH)
+    shutil.rmtree(LICENSES_DIRPATH, ignore_errors=True)
 
-{% if cookiecutter.create_remote == 'yes' %}
 
-{% if cookiecutter.remote_provider == 'GitHub' %}
-create_github_repo()
-{% elif cookiecutter.remote_provider == 'GitLab' %}
-create_gitlab_repo()
-{% endif %}
+def setup_git_repo():
+    {% if cookiecutter.gitignore != 'windows,osx,linux,git' %}
+    with open(GITIGNORE_PATH, 'wb') as f:
+        f.write(requests.get(GITIGNORE_URL))
+    print("updated '{}'".format(GITIGNORE_PATH))
+    {% endif %}
 
-run(['git', 'remote', 'add', 'origin', REPO_REMOTE_URL])
-run(['git', 'push', '-u', 'origin', 'master'])
+    run(['git', 'init'])
+    run(['git', 'status'])
+    run(['git', 'add', '-A'])
+    run(['git', 'status'])
+    run(['git', 'commit', '-m', 'Initial commit'])
 
-{% endif %}
+    {% if cookiecutter.create_remote == 'yes' %}
+    create_remote_repo()
+    run(['git', 'remote', 'add', 'origin', REMOTE_REPO_URL])
+    run(['git', 'push', '-u', 'origin', 'master'])
+    {% endif %}
 
-print
-print '{{cookiecutter.repo_name}} setup successfully!'
-print
-print
+
+def main():
+    setup_license_file()
+    setup_git_repo()
+    print('\n{{cookiecutter.cookiecutter_slug}} setup successfully!\n\n')
+
+
+if __name__ == '__main__':
+    main()
